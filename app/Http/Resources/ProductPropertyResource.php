c38719ddcf0e28fa19a2031e501b9d45da5cef37
Vladimir Ivanov <mrvovcheg@gmail.com>
2023-12-28 01:03:32 +0500

    Improve product and product property resources

--- app/Http/Resources/ProductPropertyResource.php
+++ app/Http/Resources/ProductPropertyResource.php
@@ -14,6 +14,9 @@ class ProductPropertyResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'name' => $this->name,
+            'value' => $this->value,
+        ];
     }
 }
--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -14,6 +14,11 @@ class ProductResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'count' => $this->count,
+            'properties' => ProductPropertyResource::collection($this->properties),
+        ];
     }
 }
