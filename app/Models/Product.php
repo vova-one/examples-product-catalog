3c1bcee3770bd609af34c1e966ce75059644a68b
Vladimir Ivanov <mrvovcheg@gmail.com>
2023-12-27 19:19:27 +0500

    Make product and product property models, migration and seeder

---
+++ app/Models/Product.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    public function properties(): HasMany
+    {
+        return $this->hasMany(ProductProperty::class);
+    }
+}
